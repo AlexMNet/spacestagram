556ecadeaaa3901229ddf4930581402f1585f4bc
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-01-20 02:09:12 -0800

    Update Card Component
    Add read more/read less functionality to card
    Add Author conditionally if present
    Add date

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,23 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 export default function Card({ copyright, date, explanation, title, url }) {
+  const [readMore, setReadMore] = useState('...Read More');
+  const [truncate, setTruncate] = useState(true);
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+
+    if (readMore === '...Read More') {
+      setReadMore(' Read Less');
+      setTruncate(false);
+    } else {
+      setReadMore('...Read More');
+      setTruncate(true);
+    }
+  };
+
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg mx-auto my-16'>
       <Image
@@ -15,12 +31,28 @@ export default function Card({ copyright, date, explanation, title, url }) {
         alt={title}
       />
       <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>The Coldest Sunset</div>
+        <div className='mb-2'>
+          <p className='font-bold text-xl'>{title}</p>
+          <small className='text-gray-400'>{date}</small>
+        </div>
+
         <p className='text-gray-700 text-base'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
+          {truncate
+            ? explanation.substring(0, 200)
+            : explanation.substring(0, 10000)}
+          <a
+            href='#'
+            onClick={handleOnClick}
+            className='font-bold text-blue-500'
+          >
+            {readMore}
+          </a>
         </p>
+        {copyright && (
+          <div className='mt-3 text-gray-400'>
+            <small>photo by: {copyright}</small>
+          </div>
+        )}
       </div>
       <div className='px-6 pt-4 pb-2'>
         <button className='inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2 transition ease-in-out delay-150  hover:scale-110 duration-300'>
